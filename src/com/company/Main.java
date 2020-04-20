6ac561f6f459753261e92d0722939bd0a8ee918c
Marshmellowon <leo503801@gmail.com>
2020-04-20 16:58:06 +0900

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n;
+        int i;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        i = sc.nextInt();
+    }
+}
